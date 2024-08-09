4074b6763397df4e27b54755f614f3a70231c12a
Wahyu Pranata <wahyupranata131@gmail.com>
2024-08-09 15:00:29 +0800

    Update: rollup option entry points

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,17 @@ import { sync } from "glob";
 export default {
   root: "./src",
   build: {
-    outDir: "./public",
-    emptyOutDir: true
+    outDir: path.resolve(__dirname, "./public"),
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: path.resolve('src/index.html'),
+        "ormawa-fakultas": path.resolve('src/ormawa-fakultas/index.html'),
+        "ormawa-universitas": path.resolve('src/ormawa-universitas/index.html'),
+        sejarah: path.resolve('src/sejarah/index.html'),
+        "virtual-tour": path.resolve('src/virtual-tour/index.html'),
+      }
+    }
   },
   resolve: {
     alias: [
